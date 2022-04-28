c127ec804abdcf0fc4169437fc34793db89c70d6
许要军 <xuyaojun@cdqcp.com>
2022-04-28 15:06:59 +0800

    P46_02使用vue-cli创建项目

---
+++ day03/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>欢迎使用vue-cli创建项目</h1>
+    <h2>{{ name }}--{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "许要军",
+      age: 30,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+}
+</style>
