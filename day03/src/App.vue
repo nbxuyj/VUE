83fab3f89c83ffe4a3611a87415ca17ba570b523
许要军 <xuyaojun@cdqcp.com>
2022-04-28 16:05:31 +0800

    P47_03对业务进行组件化拆分【OK】

--- day03/src/App.vue
+++ day03/src/App.vue
@@ -1,24 +1,60 @@
 <template>
   <div id="app">
-    <h1>欢迎使用vue-cli创建项目</h1>
-    <h2>{{ name }}--{{ age }}</h2>
+    <div class="header"><Header></Header></div>
+    <div class="main">
+      <div class="left"><Left></Left></div>
+      <div class="right"><Right></Right></div>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Left from "./components/Left.vue";
+import Right from "./components/Right.vue";
+
 export default {
   name: "App",
-  components: {},
+  //注册其它组件
+  components: {
+    Header,
+    Left,
+    Right,
+  },
   data() {
-    return {
-      name: "许要军",
-      age: 30,
-    };
+    return {};
   },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
 #app {
+  width: 100vw;
+  height: 100vw;
+  display: flex;
+  flex-direction: column;
+}
+#app .header {
+  height: 50px;
+  background: lightblue;
+}
+#app .main {
+  flex: 1;
+  display: flex;
+}
+
+#app .main .left {
+  width: 200px;
+  background: lightcoral;
+}
+
+#app .main .right {
+  flex: 1;
+  background: lightgray;
 }
 </style>
