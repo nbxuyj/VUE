8e4efcdc59c1e0d5a4a300e39eb649b68ed4d14e
许要军 <xuyaojun@cdqcp.com>
2022-04-29 14:58:39 +0800

    修改端口号。

--- day03/vue.config.js
+++ day03/vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:true //关闭语法检查。
+  lintOnSave:true ,//关闭语法检查。
+  devServer:{
+    port:8082,
+    open:true,
+  }
 })
