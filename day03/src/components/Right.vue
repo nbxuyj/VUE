2e78ff239c07c84908380ab0dfb32edf5a80faf7
许要军 <xuyaojun@cdqcp.com>
2022-04-30 09:35:38 +0800

    P50_06引入swiper OK。

--- day03/src/components/Right.vue
+++ day03/src/components/Right.vue
@@ -12,16 +12,16 @@
     <div id="swiper">
       <!-- swiper根主键 -->
       <swiper ref="mySwiper" :options="swiperOptions">
-              <!-- swipe子主键 -->
+        <!-- swipe子主键 -->
         <swiper-slide>
-          <img src="http://www.zjeasy.com/Content/MHWZ/img/banner2.jpg"/>
+          <img src="https://adsfile.kugou.com/b8f264829670880772dd94117c68a87e.jpg" />
         </swiper-slide>
         <swiper-slide>
-          <img src="http://www.zjeasy.com/Content/MHWZ/img/banner3.jpg"/>
-          </swiper-slide>
+          <img src="https://adsfile.kugou.com/81e81d43ae4282433fe44ed4fac1bfcd.jpg" />
+        </swiper-slide>
         <swiper-slide>
-          <img src="http://www.zjeasy.com/Content/MHWZ/img/banner1.jpg"/>
-          </swiper-slide>
+          <img src="https://adsfile.kugou.com/2c4d161f484ef144f127d357f1f45956.jpg" />
+        </swiper-slide>
 
         <div class="swiper-pagination" slot="pagination"></div>
       </swiper>
@@ -64,10 +64,41 @@ export default {
       showTable: true,
       showCharts: false,
       //swiper选项。
-      swiperOption: {
+      swiperOptions: {
+        //分页器
         pagination: {
+          //指定选择器。
           el: ".swiper-pagination",
+
+          type: 'bullets',
+          //type: 'fraction',
+          //type: "progressbar",
+          //type : 'custom',
+          //‘bullets’  圆点（默认）
+          // ‘fraction’  分式
+          // ‘progressbar’  进度条
+
+          //此参数设置为true时，点击分页器的指示点分页器会控制Swiper切换。
+          clickable: true,
+
+
+
+
         },
+
+        autoplay:true,//等同于以下设置
+        // autoplay: {
+        //   delay: 5000,
+        //   stopOnLastSlide: true, //如果设置为true，当切换到最后一个slide时停止自动切换。（loop模式下无效）。
+        //   disableOnInteraction: true, //用户操作swiper之后，是否禁止autoplay。默认为true：停止。
+        // },
+
+        //loop 设置为 true 则开启循环(loop)模式
+        loop: true,
+
+        //效果。
+        effect: 'cube',
+
       },
     };
   },
@@ -112,16 +143,20 @@ export default {
 #main {
   width: 400px;
   height: 200px;
-  border:1px solid red;
+  border: 1px solid red;
 }
 
 #swiper {
   width: 800px;
   height: 400px;
   border: 1px solid greenyellow;
+
+  /* 修改导航颜色。 */
+  --swiper-theme-color: #ff6600;
+  /* --swiper-pagination-color: #00ff33; 两种都可以 */
 }
 
-#swiper img{
+#swiper img {
   width: 100%;
 }
 </style>
