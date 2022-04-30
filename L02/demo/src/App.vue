b007b5ee22826a0b5bada9a72331fbecaf6d09b3
许要军 <xuyaojun@cdqcp.com>
2022-04-30 13:40:01 +0800

    3.判断当前路由非目标路由再跳转。

--- L02/demo/src/App.vue
+++ L02/demo/src/App.vue
@@ -10,9 +10,10 @@
     <Page3 /> -->
 
     <!-- 使用路由视图，用于展现路由匹配的组件。tag默认是a标签 注：在vue-route4中已经移除了。 -->
-        <router-link  to="/Page1">原始A标签跳转Page1</router-link> &nbsp;
+    <router-link to="/Page1">原始A标签跳转Page1</router-link> &nbsp;
     <!--方案1.使用插槽方案：但是不是想要的结果。是在a标签中嵌套button，页面结构复杂 -->
-    <router-link to="/Page1">  <button>插槽方案跳转Page1</button></router-link> &nbsp;
+    <router-link to="/Page1"> <button>插槽方案跳转Page1</button></router-link>
+    &nbsp;
     <!--方案2.手动跳转。 -->
     <button @click="gotoPage3">手动跳转Page3</button>
 
@@ -31,15 +32,17 @@ export default {
     // Page2,
     // Page3,
   },
-  methods:{
-    gotoPage3(){
+  methods: {
+    gotoPage3() {
       //this.$router 保存的是Vue实例身上添加的VueRouter对象。（路由器对象）
       //this.$route 保存的当前路由信息。
       //console.log(this.$router,this.$route);
-      //3.通过
-      this.$router.push("/page3")
-    }
-  }
+      //3.判断当前路由非目标路由再跳转。
+      if (this.$route.path != "/page3") {
+        this.$router.push("/page3");
+      }
+    },
+  },
 };
 </script>
 
