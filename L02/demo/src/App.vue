ec1a69a1cd1a969914b9b547a80204def4fdbbe9
许要军 <xuyaojun@cdqcp.com>
2022-04-30 13:34:42 +0800

    P52_08初识VueRouter

--- L02/demo/src/App.vue
+++ L02/demo/src/App.vue
@@ -1,23 +1,45 @@
 <template>
   <div id="app">
     <h1>欢迎学习vue路由课程</h1>
+    <!-- <a>页面1</a>
+    <a>页面2</a>
+    <a>页面3</a>
+    <hr />
     <Page1 />
     <Page2 />
-    <Page3 />
+    <Page3 /> -->
+
+    <!-- 使用路由视图，用于展现路由匹配的组件。tag默认是a标签 注：在vue-route4中已经移除了。 -->
+        <router-link  to="/Page1">原始A标签跳转Page1</router-link> &nbsp;
+    <!--方案1.使用插槽方案：但是不是想要的结果。是在a标签中嵌套button，页面结构复杂 -->
+    <router-link to="/Page1">  <button>插槽方案跳转Page1</button></router-link> &nbsp;
+    <!--方案2.手动跳转。 -->
+    <button @click="gotoPage3">手动跳转Page3</button>
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Page1 from "./pages/Page1.vue";
-import Page2 from "./pages/Page2.vue";
-import Page3 from "./pages/Page3.vue";
+// import Page1 from "./pages/Page1.vue";
+// import Page2 from "./pages/Page2.vue";
+// import Page3 from "./pages/Page3.vue";
 export default {
   name: "App",
   components: {
-    Page1,
-    Page2,
-    Page3,
+    // Page1,
+    // Page2,
+    // Page3,
   },
+  methods:{
+    gotoPage3(){
+      //this.$router 保存的是Vue实例身上添加的VueRouter对象。（路由器对象）
+      //this.$route 保存的当前路由信息。
+      //console.log(this.$router,this.$route);
+      //3.通过
+      this.$router.push("/page3")
+    }
+  }
 };
 </script>
 
--- L02/demo/src/main.js
+++ L02/demo/src/main.js
@@ -1,8 +1,49 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入路由。
+import VueRouter from 'vue-router'
+
+//因为VueRouter是一个插件需要Use。
+Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
+//导入路由需要的页面。
+import Page1 from "./pages/Page1.vue";
+import Page2 from "./pages/Page2.vue";
+import Page3 from "./pages/Page3.vue";
+
+//创建Vue实例。
 new Vue({
   render: h => h(App),
+
+  // 创建VueRouter对象，并通过router传给vue实例。
+  router: new VueRouter({
+    //通过routes配置具体的路由信息，每一条路由信息需要配置一个对象，至少包含path component
+    routes: [
+      {
+        //请求路由的路径
+        path:'/Page1',
+        //当前路由的组件。
+        component: Page1
+
+      },
+      {
+        //请求路由的路径
+        path:'/Page2',
+        //当前路由的组件。
+        component: Page2
+
+      },
+      {
+        //请求路由的路径
+        path:'/Page3',
+        //当前路由的组件。
+        component: Page3
+
+      }
+
+    ]
+  })
 }).$mount('#app')
