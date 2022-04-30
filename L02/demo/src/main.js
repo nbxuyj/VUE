3f3b026861b0ea03a763458c826917c68c7070f6
许要军 <xuyaojun@cdqcp.com>
2022-04-30 14:39:37 +0800

    P54_10补充一下ES6的模块化OK

--- L02/demo/src/main.js
+++ L02/demo/src/main.js
@@ -21,6 +21,24 @@ new Vue({
   router
 }).$mount('#app')
 
+//导入全部成员转为一个对象来接收。
+// import * as abc from './utils'
+// console.log(abc);
 
-import * as abc from './utils'
-console.log(abc);
+//导入指定成员。
+// import {obj2} from './utils'
+// console.log(obj2);
+
+//如果导出成员中包含default，那么默认导出的成员的key是default.
+// import * as m from './utils'
+// console.log(m.default);
+
+//注：由于default是js中的关键字，需要通as重命名。
+// import {num2, default as obj3} from './utils'
+// console.log(num2,obj3);
+
+
+//如果就是一个默认导出,在导入时，可以直接定义一个对象名来接收。
+//import { default as obj3} from './utils/index2'
+import m2 from './utils/index2'
+console.log(m2);
--- L02/demo/src/utils/index.js
+++ L02/demo/src/utils/index.js
@@ -4,26 +4,35 @@ let fun1 = function () {
     console.log("我是方法1");
 }
 
-let fun2= function () {
+let fun2 = function () {
 
     console.log("我是方法1");
 }
-let num1=100
-let num2=200
+let num1 = 100
 
-let obj1={
-    name:'张三',
+//知识点2：可以在定义时导出。
+export let num2 = 200
+
+let obj1 = {
+    name: '张三',
+    age: 30
+}
+
+let obj2 = {
+    name: '关尽',
+    age: 20
+
+}
+//知识点1：导出需要向外暴露的成员。
+// export {
+//     fun2,
+//     num1,
+//     obj2
+// }
+
+//在导出成员时，可以指定一个成员默认导出，注意：只能有一个默认导出。
+export default{
+    name:'吴变化',
     age:30
-}
 
-let obj2={
-    name:'关尽',
-    age:20
-
-}
-
-export{
-    fun2,
-    num1,
-    obj2
 }
